b16c64d7da029d7d08660795007c2e3837d2c922
Steven <steven@steventan.com.au>
2023-10-12 11:28:59 +1100

    delete listing fetch

--- src/routes/facility/+page.svelte
+++ src/routes/facility/+page.svelte
@@ -1,16 +1,1 @@
-<script>
-	export let data;
-	console.log(data);
-
-	const stations = data.station;
-</script>
-
 <h1 class="text-3xl font-bold">Facilities</h1>
-
-<ol>
-	{#each stations as { code, name }}
-		<li>
-			<a class="border-b border-b-blue-300" href={`facility/${code}`}>{name}</a>
-		</li>
-	{/each}
-</ol>
