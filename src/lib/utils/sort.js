9ebacc0f312a32d73bdd4f7ff1599cc3ab96d4e8
Steven <steven@steventan.com.au>
2023-10-18 15:03:26 +1100

    updated sitemap

---
+++ src/lib/utils/sort.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {string} prop
+ * @returns {Function}
+ */
+export function sortByProp(prop) {
+	return (
+		/**
+		 * @param {*} a
+		 * @param {*} b
+		 * @returns {number}
+		 */
+		(a, b) => {
+			const nameA = a[prop].toUpperCase(); // ignore upper and lowercase
+			const nameB = b[prop].toUpperCase(); // ignore upper and lowercase
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+
+			// names must be equal
+			return 0;
+		}
+	);
+}
