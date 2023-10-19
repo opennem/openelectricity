3cdc835eaef8c80ccec548d636925ad06c48405e
Steven <steven@steventan.com.au>
2023-10-19 17:12:14 +1100

    Update facility.types.ts

--- src/lib/types/facility.types.ts
+++ src/lib/types/facility.types.ts
@@ -3,6 +3,7 @@ import type { Unit } from './unit.types';
 
 export type Facility = {
 	_id: string;
+	name: string;
 	code: string;
 	capacity_registered: number;
 	description: PortableTextComponents;
