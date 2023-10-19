5bbb7741f43f86cfd1908f3d1c3cde21fae76c45
Steven <steven@steventan.com.au>
2023-10-19 12:37:40 +1100

    use env vars for sanity id and dataset

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,7 +1,8 @@
 import { createClient } from '@sanity/client';
+import { PUBLIC_SANITY_DATASET, PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
 export const client = createClient({
-	projectId: 'bjedimft',
-	dataset: 'production',
+	projectId: PUBLIC_SANITY_PROJECT_ID,
+	dataset: PUBLIC_SANITY_DATASET,
 	apiVersion: '2023-10-11',
 	useCdn: false
 });
