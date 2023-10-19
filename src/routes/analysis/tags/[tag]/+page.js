0133d83210ee66857c8fb73fb667784a094a0efc
Gerard Mason <gerard.mason@superpowerinstitute.com.au>
2023-10-19 15:17:16 +1100

    optimised the tag listing query some, added lookup for the actual tag title

--- src/routes/analysis/tags/[tag]/+page.js
+++ src/routes/analysis/tags/[tag]/+page.js
@@ -4,12 +4,15 @@ import { client } from '$lib/sanity';
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
 	const data = await client.fetch(
-		`*[_type == "article" && count((tags[]->slug.current)[@ in ["${params.tag}"]]) > 0] { title, slug }`
+		`*[_type == "tag" && slug.current == "${params.tag}"]{
+			title, 
+			"articles": *[_type=='article' && count((tags[]->slug.current)[@ == "${params.tag}"]) > 0]{ title, slug }
+		}`
 	);
 
 	if (data) {
 		return {
-			articles: data
+			...data[0]
 		};
 	}
 
--- src/routes/analysis/tags/[tag]/+page.svelte
+++ src/routes/analysis/tags/[tag]/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-    import { page } from '$app/stores';
 	export let data;
-	let articles = data.articles;
-    let tag = $page.params.tag
 </script>
 
-<h1 class="text-3xl font-bold">Articles for tag <em>{tag}</em></h1>
+<h1 class="text-3xl font-bold">Articles for tag <em>{data.title}</em></h1>
 <div>
-	{#each articles as { title, slug }}
+	{#each data.articles as { title, slug }}
         <div>
             <h3 class="text-3xl font-bold">{title}</h3>
             <a href="/analysis/{slug.current}">Read more</a>
