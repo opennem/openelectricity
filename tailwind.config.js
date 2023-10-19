57aff7258974cb0d50f76cfa054fb03ef04f98b0
Steven <steven@steventan.com.au>
2023-10-19 12:24:16 +1100

    add DM sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['DM Sans', 'sans-serif']
+		},
 		extend: {}
 	},
 	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
