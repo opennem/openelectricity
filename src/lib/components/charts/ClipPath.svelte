61fa856d8d8b862cc2813620aa98d75c50595d16
Steven <steven@steventan.com.au>
2024-03-06 17:01:13 +1100

    Create ClipPath.svelte

---
+++ src/lib/components/charts/ClipPath.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { getContext } from 'svelte';
+
+	const { width, height, padding } = getContext('LayerCake');
+
+	export let id = 'clipping-path';
+</script>
+
+<clipPath {id} transform="translate({-$padding.left}, 0)">
+	<rect
+		width={$width + $padding.left + $padding.right}
+		height={$height + $padding.top + $padding.bottom}
+		fill="transparent"
+	/>
+</clipPath>
