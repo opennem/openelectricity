b05c86de41f2d2ca4ca17427a6cf8582879f33aa
Steven <steven@steventan.com.au>
2024-03-13 17:21:35 +1100

    update card styles

--- src/lib/components/articles/AnalysisCard.svelte
+++ src/lib/components/articles/AnalysisCard.svelte
@@ -1,19 +1,77 @@
 <script>
+	import { format, parse } from 'date-fns';
 	import { urlFor } from '$lib/sanity';
 
-	/** @type {import('$lib/types/article.types').Article} */
+	/** @type {Article} */
 	export let article;
+
+	$: hasCover = article.cover;
+	$: headerStyles = hasCover ? 'my-8 h-42' : 'my-8 px-6 h-42';
+	$: linkStyles = hasCover
+		? `grid grid-cols-1 gap-4 content-between border-y border-warm-grey text-dark-grey hover:no-underline ${$$restProps.class}`
+		: `bg-light-warm-grey grid grid-cols-1 gap-4 content-between border border-warm-grey rounded-lg text-dark-grey hover:no-underline ${$$restProps.class}`;
+	$: authorStyles = hasCover
+		? 'flex items-center gap-8 justify-between mt-6'
+		: 'flex items-center gap-8 justify-between px-6 my-6';
+
+	$: console.log('article', article);
+	const publishedDate = parse(article.publish_date, 'yyyy-MM-dd', new Date());
 </script>
 
-<a
-	href={`/analysis/${article.slug.current}`}
-	class={`flex flex-col h-full py-8 ${$$restProps.class}`}
->
-	<h3>{article.title}</h3>
-	<div>
-		{#each article.author as author} <div>{author.name}</div>{/each}
+<a href={`/analysis/${article.slug.current}`} class={linkStyles}>
+	<header class={headerStyles}>
+		<div class="text-xs flex items-center justify-between">
+			<span class="px-2 rounded-full bg-warm-grey text-dark-grey">Analysis</span>
+			<span>{format(publishedDate, 'dd MMM yyyy')}</span>
+		</div>
+
+		<h3 class="font-medium mt-8 line-clamp-4">
+			{article.title}
+		</h3>
+	</header>
+
+	<div class="grid grid-cols-1 gap-4">
+		<div class={authorStyles}>
+			<div class="flex items-center">
+				{#each article.author as author, i}
+					<span class={`w-16 h-16 block grayscale relative ${i > 0 ? '-left-2' : 'left-0'}`}>
+						<img
+							class="rounded-full border border-white"
+							src={urlFor(author.image).width(100).height(100).url()}
+							alt={author.image.alt}
+						/>
+					</span>
+				{/each}
+			</div>
+
+			<div class="author flex flex-wrap justify-end gap-1 text-xs text-mid-grey">
+				{#each article.author as author, i}
+					{#if i > 0}
+						<span>+</span>
+					{/if}
+					<span>{author.name}</span>
+				{/each}
+			</div>
+		</div>
+		{#if hasCover}
+			<img
+				class="rounded-b-lg"
+				src={urlFor(article.cover).width(590).height(346).url()}
+				alt={article.cover.alt}
+			/>
+		{/if}
 	</div>
-	{#if article.cover}
-		<img src={urlFor(article.cover).width(590).height(346).url()} alt={article.cover.alt} />
-	{/if}
 </a>
+
+<style>
+	a:hover h3 {
+		text-decoration: underline;
+	}
+	h3 {
+		font-size: 2rem;
+		line-height: 2.4rem;
+	}
+	.author {
+		line-height: 1.2rem;
+	}
+</style>
--- src/lib/components/articles/MilestoneCard.svelte
+++ src/lib/components/articles/MilestoneCard.svelte
@@ -1,20 +1,48 @@
 <script>
+	import { format, parse } from 'date-fns';
 	import { urlFor } from '$lib/sanity';
+	import FuelTechTag from '$lib/components/FuelTechTag.svelte';
 
-	/** @type {import('$lib/types/article.types').Article} */
+	/** @type {Article} */
 	export let article;
+
+	$: console.log('article', article);
+	const publishedDate = parse(article.publish_date, 'yyyy-MM-dd', new Date());
 </script>
 
 <a
 	href={`/analysis/${article.slug.current}`}
-	class={`flex flex-col h-full py-8 ${$$restProps.class}`}
+	class={`grid grid-cols-1 content-between border border-warm-grey rounded-lg text-dark-grey hover:no-underline ${$$restProps.class}`}
 >
-	<h3>{article.title}</h3>
+	<header class="mt-8">
+		<div class="flex items-center justify-between px-8">
+			<FuelTechTag fueltech={article.fueltech}>Milestone</FuelTechTag>
+			<span class="text-xs">{format(publishedDate, 'dd MMM yyyy')}</span>
+		</div>
+
+		<h3 class="font-medium px-8 mt-8">{article.title}</h3>
+	</header>
+
 	{#if article.cover}
-		<img src={urlFor(article.cover).width(590).height(346).url()} alt={article.cover.alt} />
+		<img
+			class="rounded-b-lg"
+			src={urlFor(article.cover).width(590).height(346).url()}
+			alt={article.cover.alt}
+		/>
+	{:else}
+		<div
+			style={`background-image: url(/img/records/${article.fueltech}.png)`}
+			class="h-[172px] bg-no-repeat bg-top bg-cover rounded-b-lg"
+		/>
 	{/if}
-	<div
-		style={`background-image: url(/img/records/${article.fueltech}.png)`}
-		class={`h-44 bg-no-repeat bg-top bg-cover`}
-	/>
 </a>
+
+<style>
+	a:hover h3 {
+		text-decoration: underline;
+	}
+	h3 {
+		font-size: 2rem;
+		line-height: 2.4rem;
+	}
+</style>
