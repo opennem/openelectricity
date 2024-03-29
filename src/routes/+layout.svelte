6bee4b643b72cec2303f54d13807ef390a4e5d61
Steven <steven@steventan.com.au>
2024-03-29 14:52:59 +1100

    add page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,22 @@
 	import '../app.css';
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		// If the browser doesn't support view transitions, return early
+		// @ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <Nav />
