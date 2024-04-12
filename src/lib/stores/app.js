e2067d4f35d2f96d17b93d5359e813bc9eafd56c
Steven <steven@steventan.com.au>
2024-04-12 22:53:17 +1000

    update banner

--- src/lib/stores/app.js
+++ src/lib/stores/app.js
@@ -1,6 +1,38 @@
-import { readable } from 'svelte/store';
+import { readable, writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+// Get value from localStorage if in browser and the value is stored, otherwise fallback
+function fromLocalStorage(storageKey, fallbackValue) {
+	if (browser) {
+		const storedValue = window.localStorage.getItem(storageKey);
+
+		if (storedValue !== 'undefined' && storedValue !== null) {
+			return typeof fallbackValue === 'object'
+				? JSON.parse(storedValue)
+				: storedValue === 'true'
+				? true
+				: storedValue === 'false'
+				? false
+				: storedValue;
+		}
+	}
+
+	return fallbackValue;
+}
+function toLocalStorage(store, storageKey) {
+	if (browser) {
+		store.subscribe((value) => {
+			let storageValue = typeof value === 'object' ? JSON.stringify(value) : value;
+
+			window.localStorage.setItem(storageKey, storageValue);
+		});
+	}
+}
 
 /** @type {import('svelte/store').Readable<string>} */
 export const dataTrackerLink = readable(
 	'https://feature-234-site-design-upda.opennem-fe-bl7.pages.dev/'
 );
+
+export const bannerOpen = writable(fromLocalStorage('bannerOpen', true));
+toLocalStorage(bannerOpen, 'bannerOpen');
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
+	import { fade } from 'svelte/transition';
+	import { onNavigate } from '$app/navigation';
 	import '../app.css';
+
 	import Nav from '$lib/components/Nav.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import IconXMark from '$lib/icons/XMark.svelte';
 
-	import { onNavigate } from '$app/navigation';
+	import { bannerOpen } from '$lib/stores/app';
 
 	onNavigate((navigation) => {
 		// If the browser doesn't support view transitions, return early
@@ -20,20 +24,27 @@
 	});
 </script>
 
-<div class="w-full bg-black text-white text-sm px-8 py-6 font-light md:flex gap-3 justify-center">
-	<div>
-		<strong class="font-semibold">OpenNEM</strong> is now
-		<strong class="font-semibold">Open Electricity</strong>.
-	</div>
-	<div>
-		<a
-			href="https://openelectricity.org.au/analysis/from-opennem-to-open-electricity"
-			class="underline text-white"
+{#if $bannerOpen}
+	<div
+		class="relative w-full bg-black text-white text-sm px-8 py-6 font-light md:flex gap-3 justify-center"
+	>
+		<div>
+			<strong class="font-semibold">OpenNEM</strong> is now
+			<strong class="font-semibold">Open Electricity</strong>.
+		</div>
+		<div>
+			<a
+				href="https://openelectricity.org.au/analysis/from-opennem-to-open-electricity"
+				class="underline text-white"
+			>
+				Read
+			</a> about the update.
+		</div>
+		<button class="absolute right-12 top-10 md:top-6" on:click={() => ($bannerOpen = false)}
+			><IconXMark classes="w-8 h-8" /></button
 		>
-			Read
-		</a> about the update.
 	</div>
-</div>
+{/if}
 
 <Nav />
 
