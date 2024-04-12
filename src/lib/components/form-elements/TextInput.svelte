28938818d40adf74ede76cadab8b921329844ab0
Steven <steven@steventan.com.au>
2024-04-12 11:47:31 +1000

    adjusted styles

--- src/lib/components/form-elements/TextInput.svelte
+++ src/lib/components/form-elements/TextInput.svelte
@@ -8,7 +8,7 @@
 
 <input
 	{type}
-	class={`rounded-md border-solid border-[0.05rem] border-mid-warm-grey p-4 font-sans text-sm placeholder:text-mid-warm-grey focus:ring-0 focus:border-dark-grey ${$$restProps.class}`}
+	class={`rounded-md border-solid placeholder:font-light border-[0.05rem] border-mid-warm-grey p-4 font-sans text-sm placeholder:text-mid-warm-grey focus:ring-0 focus:border-dark-grey ${$$restProps.class}`}
 	{placeholder}
 	{value}
 	on:input|preventDefault={changeHandler}
