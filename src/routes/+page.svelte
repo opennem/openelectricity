a7c42a6b15bed4e787615d97812b9d8434455303
Steven <steven@steventan.com.au>
2024-04-17 15:36:14 +1000

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -198,7 +198,7 @@
 		<div class="container max-w-none lg:container">
 			<header class="flex justify-between items-center mb-8">
 				<h3>{analysis_title}</h3>
-				<SectionLink href="/analysis" title="View all analyses" />
+				<!-- <SectionLink href="/analysis" title="View all analyses" /> -->
 			</header>
 			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
 				{#each analysisArticles as article}
