cd2973e4ed61602f1e1a0e1797d5a76405df295c
Steven <steven@steventan.com.au>
2024-07-01 10:41:23 +1000

    Update AreaStacked.svelte

--- src/lib/components/charts/elements/AreaStacked.svelte
+++ src/lib/components/charts/elements/AreaStacked.svelte
@@ -24,8 +24,8 @@
 	$: areaGen = area()
 		.x((d) => $xGet(d))
 		.y0((d) => $yScale(d[0]))
-		.y1((d) => $yScale(d[1]))
-		.defined((d) => d[0] !== 0 || d[1] !== 0);
+		.y1((d) => $yScale(d[1]));
+	// .defined((d) => d[0] !== 0 || d[1] !== 0);
 
 	$: lineGen = line(
 		(d) => $xGet(d),
