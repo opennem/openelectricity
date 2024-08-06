8b93ff22f28174324e8dc48f8c6e1eddb04d00f0
Steven <steven@steventan.com.au>
2024-08-06 10:39:48 +1000

    Update +server.js

--- src/routes/api/records/+server.js
+++ src/routes/api/records/+server.js
@@ -3,6 +3,7 @@ import { PUBLIC_RECORDS_API, PUBLIC_API_KEY } from '$env/static/public';
 
 export async function GET({ url, fetch, setHeaders }) {
 	setHeaders({
+		'cache-control': 'max-age=0',
 		Authorization: `Bearer ${PUBLIC_API_KEY}`
 	});
 
