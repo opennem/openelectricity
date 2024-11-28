046ba0e8fb83c5d9147636ef98e67fd05c42aa6a
Steven <steven@steventan.com.au>
2024-11-28 13:42:33 +1100

    setup to query ember bridge api

--- src/routes/studio/lens/+page.svelte
+++ src/routes/studio/lens/+page.svelte
@@ -1,1 +1,46 @@
-<h1>Lens</h1>
+<script>
+	import PageHeaderSimple from '$lib/components/PageHeaderSimple.svelte';
+	import Meta from '$lib/components/Meta.svelte';
+	import Filters from './components/Filters.svelte';
+
+	export let data;
+
+	let error = false;
+	let errorMsg = '';
+
+	$: console.log(data);
+	$: countries = data.countries;
+	$: if (data.error) {
+		console.error(data.error);
+		error = true;
+		errorMsg = data.error;
+	}
+</script>
+
+<Meta
+	title="The Studio — Lens"
+	description="Lens visualises energy and emissions data from regions around the world using Open Electricity's visualisation components."
+	image="/img/preview.jpg"
+/>
+
+<PageHeaderSimple>
+	<div slot="main-heading">
+		<h1 class="tracking-widest text-center">Lens</h1>
+	</div>
+	<div slot="sub-heading">
+		<p class="text-sm text-center w-full md:w-[800px] mx-auto">
+			Charts from Open Electricity. Data from
+			<a href="https://ember-energy.org/" target="_blank">Ember</a>.
+		</p>
+	</div>
+</PageHeaderSimple>
+
+{#if error}
+	<div class="md:container py-12 text-center">
+		<p class="text-dark-red font-semibold">{errorMsg}</p>
+	</div>
+{:else}
+	<section class="md:container py-12">
+		<Filters {countries} />
+	</section>
+{/if}
---
+++ src/routes/studio/lens/components/Filters.svelte
@@ -0,0 +1,21 @@
+<script>
+	import FormSelect from '$lib/components/form-elements/Select.svelte';
+
+	export let countries;
+
+	$: countryOptions = countries.map((country) => ({
+		label: country.name,
+		value: country.iso
+	}));
+
+	let selectedCountry = '';
+</script>
+
+<FormSelect
+	formLabel="Region / Country"
+	options={countryOptions}
+	selected={selectedCountry}
+	paddingX="px-7"
+	paddingY="py-3"
+	on:change={(evt) => (selectedCountry = evt.detail.value)}
+/>
