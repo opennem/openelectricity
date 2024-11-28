4e24dc441683907ed264303edb05fbb76d7cd55d
Steven <steven@steventan.com.au>
2024-11-28 11:03:49 +1100

    studio homepage setup

--- src/routes/studio/+page.svelte
+++ src/routes/studio/+page.svelte
@@ -15,15 +15,30 @@
 	</div>
 	<div slot="sub-heading">
 		<p class="text-sm text-center w-full md:w-[800px] mx-auto">
-			The Studio is a space dedicated to the experimental exploration of data visualisation. It
-			serves as a sandbox for developing innovative projects that push the boundaries of how data is
-			understood and presented. While some of these projects may eventually be integrated into Open
-			Electricity, others may evolve into standalone initiatives.
-		</p>
-		<p class="text-sm text-center w-full md:w-[800px] mx-auto">
-			The primary goal is to experiment with new ways of visualising and interpreting data, with
-			valuable insights and technical advancements feeding back into Open Electricity’s core
-			features, improving both its functionality and user experience.
+			The Studio is a space dedicated to the experimental exploration of data projects. While some
+			of these may eventually be integrated into Open Electricity, others may evolve into standalone
+			initiatives.
 		</p>
 	</div>
 </PageHeaderSimple>
+
+<section class="md:container py-12">
+	<div class="grid grid-cols-1 md:grid-cols-4 gap-8">
+		<div class="border border-mid-warm-grey p-8 rounded-xl">
+			<h2 class="text-lg font-bold font-space">Records</h2>
+			<p class="text-sm">
+				Records tracks significant events in the history of the Australian electricity market.
+			</p>
+			<a href="/records" class="font-space">View Project</a>
+		</div>
+
+		<div class="border border-mid-warm-grey p-8 rounded-xl">
+			<h2 class="text-lg font-bold font-space">Lens</h2>
+			<p class="text-sm">
+				Lens visualises energy and emissions data from regions around the world using Open
+				Electricity's visualisation components.
+			</p>
+			<a href="/studio/lens" class="font-space">View Project</a>
+		</div>
+	</div>
+</section>
