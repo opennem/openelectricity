8275d7550e5e100727909a3f2dbf3df874282a9a
Steven Tan <steven@steventan.com.au>
2025-03-14 09:23:03 +1100

    show region filters

--- src/routes/records/components/FilterTags.svelte
+++ src/routes/records/components/FilterTags.svelte
@@ -1,14 +1,38 @@
 <script>
 	import { getContext } from 'svelte';
 	import IconXMark from '$lib/icons/XMark.svelte';
+	import { regionsWithLabels } from '$lib/regions';
+
 	import { fuelTechLabel, milestoneTypeLabel, periodLabel } from '../page-data-options/filters';
 
-	const { selectedFuelTechs, selectedMetrics, selectedPeriods } = getContext('records-filters');
+	const { selectedRegions, selectedFuelTechs, selectedMetrics, selectedPeriods } =
+		getContext('records-filters');
 </script>
 
 {#if $selectedFuelTechs.length || $selectedMetrics.length || $selectedPeriods.length}
 	<h4 class="hidden md:block">Filters</h4>
 {/if}
+
+<div>
+	{#if $selectedRegions.length}
+		<h6 class="md:mt-10">Region</h6>
+	{/if}
+	<div class="flex flex-col md:flex-row gap-2 flex-wrap">
+		{#each $selectedRegions as region}
+			<div
+				class="bg-white border border-warm-grey text-xs leading-xs rounded-full flex justify-between items-center gap-3 pl-5 py-1 md:py-0"
+			>
+				<span class="whitespace-nowrap">{regionsWithLabels[region]}</span>
+				<button
+					class="bg-light-warm-grey hover:bg-warm-grey rounded-full p-2 text-mid-grey"
+					onclick={() => ($selectedRegions = $selectedRegions.filter((d) => d !== region))}
+				>
+					<IconXMark class="size-6" />
+				</button>
+			</div>
+		{/each}
+	</div>
+</div>
 
 <div>
 	{#if $selectedFuelTechs.length}
