b609858f070a9a23ea783ba8f76b0f148028bdf1
Steven Tan <steven@steventan.com.au>
2025-03-25 22:23:20 +1100

    rename api

--- src/routes/(main)/records/[id]/+page.svelte
+++ src/routes/(main)/records/[id]/+page.svelte
@@ -169,7 +169,7 @@
 <Meta
 	title={pageTitle}
 	description="Track historical and current records of Australia's electricity grid with Open Electricity's record tracker"
-	image={`/api/cloudflare/screenshot?key=${data.record_id}~${focusDateTime}`}
+	image={`https://openelectricity.org.au/api/record-preview?key=${data.record_id}~${focusDateTime}`}
 />
 
 {#if data.record_id}
