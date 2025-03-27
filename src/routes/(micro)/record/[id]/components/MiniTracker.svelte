55109b341f86cc90823b704d7581a3a8eaf27c63
Steven Tan <steven@steventan.com.au>
2025-03-27 23:44:25 +1100

    updates

--- src/routes/(micro)/record/[id]/components/MiniTracker.svelte
+++ src/routes/(micro)/record/[id]/components/MiniTracker.svelte
@@ -4,56 +4,52 @@
 	import nighttimes from '$lib/utils/nighttimes';
 	import LensChart from '$lib/components/charts/LensChart.svelte';
 	import ChartStore from '$lib/components/charts/stores/chart.svelte.js';
-	import { microRecordState } from '../state.svelte';
 	import { chartOptions } from '../../../../(main)/records/[id]/MiniTracker/helpers/config';
 	import xTickValueFormatters from '../../../../(main)/records/[id]/MiniTracker/helpers/xtick-value-formatters';
 
-	let data = $props();
-	let { record, focusTime, timeSeries, metric, period, timeZone } = $derived(data);
+	let { record, focusTime, timeSeries, metric, period, timeZone } = $props();
 	let focusOn = $derived(focusTime ? new Date(focusTime) : undefined);
 	let chartCxt = init();
 
-	$effect(() => {
-		if (timeSeries.length > 0) {
-			let dataMetric = /** @type {import('openelectricity').DataMetric} */ (metric);
-			let isIntervalPeriod = period === 'interval';
+	if (timeSeries.length > 0) {
+		let dataMetric = /** @type {import('openelectricity').DataMetric} */ (metric);
+		let isIntervalPeriod = period === 'interval';
 
-			chartCxt.chartOptions.prefix = chartOptions[dataMetric].prefix;
-			chartCxt.chartOptions.displayPrefix = chartOptions[dataMetric].displayPrefix;
-			chartCxt.chartOptions.allowedPrefixes = chartOptions[dataMetric].allowedPrefixes;
-			chartCxt.chartOptions.baseUnit = chartOptions[dataMetric].baseUnit;
+		chartCxt.chartOptions.prefix = chartOptions[dataMetric].prefix;
+		chartCxt.chartOptions.displayPrefix = chartOptions[dataMetric].displayPrefix;
+		chartCxt.chartOptions.allowedPrefixes = chartOptions[dataMetric].allowedPrefixes;
+		chartCxt.chartOptions.baseUnit = chartOptions[dataMetric].baseUnit;
 
-			chartCxt.xTicks = xTickValueFormatters[record.period].ticks;
-			chartCxt.formatTickX = xTickValueFormatters[record.period].formatTick;
-			chartCxt.formatX = xTickValueFormatters[record.period].format;
-			chartCxt.timeZone = timeZone;
-			chartCxt.seriesData = timeSeries;
-			chartCxt.seriesNames = ['value'];
-			chartCxt.seriesColours = { value: fuelTechColourMap[record.fueltech_id || 'demand'] };
-			chartCxt.seriesLabels = { value: '' };
-			chartCxt.focusTime = focusOn?.getTime() || record.time;
-			chartCxt.chartTooltips.valueKey = 'value';
-			// chartCxt.useFormatY = true;
-			// chartCxt.formatY = () => '';
-			chartCxt.chartStyles.xTextClasses = 'text-lg text-mid-warm-grey';
-			chartCxt.chartStyles.xAxisYTick = 23;
+		chartCxt.xTicks = xTickValueFormatters[record.period].ticks;
+		chartCxt.formatTickX = xTickValueFormatters[record.period].formatTick;
+		chartCxt.formatX = xTickValueFormatters[record.period].format;
+		chartCxt.timeZone = timeZone;
+		chartCxt.seriesData = timeSeries;
+		chartCxt.seriesNames = ['value'];
+		chartCxt.seriesColours = { value: fuelTechColourMap[record.fueltech_id || 'demand'] };
+		chartCxt.seriesLabels = { value: '' };
+		chartCxt.focusTime = focusOn?.getTime() || record.time;
+		chartCxt.chartTooltips.valueKey = 'value';
+		// chartCxt.useFormatY = true;
+		// chartCxt.formatY = () => '';
+		chartCxt.chartStyles.xTextClasses = 'text-lg text-mid-warm-grey';
+		chartCxt.chartStyles.xAxisYTick = 23;
 
-			if (timeSeries && isIntervalPeriod) {
-				chartCxt.shadingData = nighttimes(
-					timeSeries[0].date,
-					timeSeries[timeSeries.length - 1].date,
-					timeZone
-				);
-				chartCxt.shadingFill = '#33333311';
-			}
-
-			if (isIntervalPeriod) {
-				chartCxt.chartOptions.setSmoothCurve();
-			} else {
-				chartCxt.chartOptions.setStepCurve();
-			}
+		if (timeSeries && isIntervalPeriod) {
+			chartCxt.shadingData = nighttimes(
+				timeSeries[0].date,
+				timeSeries[timeSeries.length - 1].date,
+				timeZone
+			);
+			chartCxt.shadingFill = '#33333311';
 		}
-	});
+
+		if (isIntervalPeriod) {
+			chartCxt.chartOptions.setSmoothCurve();
+		} else {
+			chartCxt.chartOptions.setStepCurve();
+		}
+	}
 
 	function init(chartHeight = 280) {
 		let chartKey = Symbol('mini-tracker');
--- src/routes/(micro)/record/[id]/state.svelte.js
+++
@@ -1,6 +0,0 @@
-export class MicroRecordState {
-	/** @type {{ date: Date, time: number, value: number } | undefined} */
-	focusRecord = $state(undefined);
-}
-
-export const microRecordState = new MicroRecordState();
