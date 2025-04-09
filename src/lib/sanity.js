230e09b10f8881610d0b3847127a510586e9ebe2
Steven Tan <steven@steventan.com.au>
2025-04-09 10:46:18 +1000

    update sanity.js

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,12 +1,13 @@
 import { createClient } from '@sanity/client';
 import { PUBLIC_SANITY_DATASET, PUBLIC_SANITY_PROJECT_ID } from '$env/static/public';
+import imageUrlBuilder from '@sanity/image-url';
+
 export const client = createClient({
 	projectId: PUBLIC_SANITY_PROJECT_ID,
 	dataset: PUBLIC_SANITY_DATASET,
 	apiVersion: '2023-10-11',
 	useCdn: false
 });
-import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder(client);
 
