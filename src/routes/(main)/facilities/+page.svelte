ee0b55bf7238bd83685a69ca4bd1d91017863e1d
Steven Tan <steven@steventan.com.au>
2026-01-23 16:20:16 +1100

    Make facility detail panel full height on mobile

--- src/routes/(main)/facilities/+page.svelte
+++ src/routes/(main)/facilities/+page.svelte
@@ -609,9 +609,9 @@
 		<!-- Facility detail panel -->
 		{#if selectedFacilityCode}
 			<div
-				class="absolute bottom-0 right-0 w-full md:bottom-6 md:right-6 md:w-[calc(50%-2rem)] bg-white md:rounded-xl md:shadow-lg z-20 flex flex-col overflow-hidden {isFullscreen
-					? 'h-2/3 md:h-[calc(66.67%-1.5rem)]'
-					: 'h-1/2 md:h-[calc(50%-1.5rem)]'}"
+				class="absolute bottom-0 right-0 w-full md:bottom-6 md:right-6 md:w-[calc(50%-2rem)] bg-white md:rounded-xl md:shadow-lg z-20 flex flex-col overflow-hidden h-full {isFullscreen
+					? 'md:h-[calc(66.67%-1.5rem)]'
+					: 'md:h-[calc(50%-1.5rem)]'}"
 				transition:fly={{ x: 400, duration: 250, easing: quintOut }}
 			>
 				<!-- Header -->
