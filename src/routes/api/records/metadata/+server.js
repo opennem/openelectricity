8803fb741d321f765e04c0e40fa412b971f1d84f
Steven Tan <steven@steventan.com.au>
2026-03-06 15:37:33 +1100

    Delete unused /api/power and /api/facilities/[code] routes, add docs to /api/records/metadata

--- src/routes/api/records/metadata/+server.js
+++ src/routes/api/records/metadata/+server.js
@@ -1,5 +1,18 @@
 import { PUBLIC_RECORDS_API } from '$env/static/public';
 
+/**
+ * Proxies the records/milestones metadata endpoint.
+ *
+ * Returns a JSON object with:
+ * - aggregates: ['low', 'high']
+ * - milestone_type: ['power', 'energy', 'demand', 'price', 'market_value', 'emissions', 'renewable_proportion']
+ * - periods: ['interval', 'day', 'week', '7d', 'month', 'quarter', 'season', 'year', 'financial_year']
+ * - units: definitions with label, unit string, and output_format for each milestone type
+ * - fueltechs: available fuel technologies
+ * - networks: NEM and WEM with timezone, interval config, subnetworks, and regions
+ *
+ * TODO: Use this to dynamically populate filters on the Records page
+ */
 export function GET({ fetch }) {
 	const path = `${PUBLIC_RECORDS_API}/metadata`;
 
