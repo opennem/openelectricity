a8b8f352a890257deacd33624f499e2e92e3ac5f
Steven Tan <steven@steventan.com.au>
2026-04-02 12:23:32 +1100

    Add auto-rendering of unknown unit fields and Sanity Studio edit links
    Switch GROQ unit query to spread operator so new schema fields appear
    automatically. Fields not explicitly handled are auto-rendered with
    editable primitives and resolved references. Adds "Edit in Sanity"
    links for facilities and units, URL detection in kv display, and a
    new /api/cms/resolve-refs endpoint for resolving reference names.

--- src/routes/(main)/studio/cms-facilities/[[facility_code]]/[[unit_code]]/+page.server.js
+++ src/routes/(main)/studio/cms-facilities/[[facility_code]]/[[unit_code]]/+page.server.js
@@ -8,19 +8,9 @@ const FACILITIES_QUERY = `*[_type == "facility"] | order(name asc) {
 	owners[]->{_id, name, legal_name, website, wikipedia, contact_email, contact_phone, address},
 	photos,
 	units[]->{
-		_id, code, dispatch_type, status, capacity_registered, capacity_maximum,
-		storage_capacity, min_generation_capacity, grid_forming, marginal_loss_factor,
-		emissions_factor_co2, emissions_factor_source,
-		data_first_seen, data_last_seen,
-		commissioning_confirmed, expected_operation_date, expected_operation_date_specificity,
-		expected_closure_date, expected_closure_date_specificity,
-		commencement_date, commencement_date_specificity,
-		closure_date, closure_date_specificity,
-		construction_start_date, construction_cost,
-		cis_tender_recipient,
+		...,
 		fuel_technology->{_id, code, name, colour, renewable, dispatch_type},
-		unit_types[]->{_id, unit_number, unit_size, capacity, unit_brand, unit_model, unit_model_year, unit_model_url, unit_height, unit_weight, mounting_type, unit_efficiency},
-		metadata_array
+		unit_types[]->{_id, unit_number, unit_size, capacity, unit_brand, unit_model, unit_model_year, unit_model_url, unit_height, unit_weight, mounting_type, unit_efficiency}
 	}
 }`;
 
