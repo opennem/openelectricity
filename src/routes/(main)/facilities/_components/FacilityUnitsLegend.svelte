13f55fc6bacdf9089ad801ebc17212e73244bd1c
Steven Tan <steven@steventan.com.au>
2026-04-14 18:36:24 +1000

    Sort units legend by fuel-tech order then unit code

--- src/routes/(main)/facilities/_components/FacilityUnitsLegend.svelte
+++ src/routes/(main)/facilities/_components/FacilityUnitsLegend.svelte
@@ -9,7 +9,10 @@
 
 	import { buildUnitColourMap } from '$lib/components/charts/facility/helpers.js';
 	import { fuelTechColourMap } from '$lib/theme/openelectricity';
+	import detailedGroup from '$lib/fuel-tech-groups/detailed';
 	import { getNumberFormat } from '$lib/utils/formatters';
+
+	const fuelTechOrder = detailedGroup.order;
 
 	const formatter0 = getNumberFormat(0);
 	const formatter1 = getNumberFormat(1);
@@ -35,13 +38,25 @@
 	}
 
 	let unitColourMap = $derived(buildUnitColourMap(units, getFuelTechColor));
+
+	// Sort by fuel-tech display order (from detailedGroup) then by unit code.
+	let sortedUnits = $derived.by(() => {
+		return [...units].sort((/** @type {any} */ a, /** @type {any} */ b) => {
+			const aPos = fuelTechOrder.indexOf(a.fueltech_id);
+			const bPos = fuelTechOrder.indexOf(b.fueltech_id);
+			const aOrder = aPos === -1 ? 999 : aPos;
+			const bOrder = bPos === -1 ? 999 : bPos;
+			if (aOrder !== bOrder) return aOrder - bOrder;
+			return (a.code ?? '').localeCompare(b.code ?? '');
+		});
+	});
 </script>
 
 {#if units?.length}
 	<div
 		class="flex overflow-x-auto px-6 py-3 divide-x divide-warm-grey scrollbar-thin scrollbar-thumb-warm-grey scrollbar-track-transparent"
 	>
-		{#each units as unit (unit.code)}
+		{#each sortedUnits as unit (unit.code)}
 			{@const colour = unitColourMap[unit.code] ?? '#888'}
 			{@const capacity = unit.capacity_maximum ?? unit.capacity_registered}
 			{@const isRetired = unit.status_id === 'retired'}
